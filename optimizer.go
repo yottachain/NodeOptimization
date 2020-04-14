a2df3a0e89e0af4c37b486bf3a857718a711937d
luya <1456887752@qq.com>
2020-04-14 18:27:35 +0800

    update

--- optimizer.go
+++ optimizer.go
@@ -6,12 +6,12 @@ import (
 
 type Optmizer struct {
 	*counter.Counter
-	GetSource func(row counter.NodeCountRow) int64
+	GetScore func(row counter.NodeCountRow) int64
 }
 
 type ResRow struct {
 	ID string
-	Source int64
+	Score int64
 }
 type ResRowList []ResRow
 
@@ -19,7 +19,7 @@ func (rrl ResRowList)Sort()ResRowList{
 
 	for i:=0;i<len(rrl);i++{
 		for j:=i;j<len(rrl);j++{
-			if rrl[j].Source>rrl[i].Source {
+			if rrl[j].Score>rrl[i].Score {
 				temp := rrl[i]
 				rrl[i]=rrl[j]
 				rrl[j]=temp
@@ -34,7 +34,7 @@ func (opt *Optmizer)Feedback(row counter.InRow){
 	opt.PushRow(row)
 }
 
-func defaultGetSource(row counter.NodeCountRow) int64 {
+func defaultGetScore(row counter.NodeCountRow) int64 {
 	var w1 int64 = 2
 	var w2 int64 = -1
 	if (row[0]+row[1])==0 {
@@ -48,12 +48,20 @@ func (opt *Optmizer)Get(ids ...string) ResRowList{
 	res := make([]ResRow,0)
 
 	for k,v:= range opt.CurrentCount(ids...) {
-		res = append(res,ResRow{k,opt.GetSource(v)})
+		res = append(res,ResRow{k,opt.GetScore(v)})
 	}
 
 	return res
 }
+func (opt *Optmizer)Get2(ids...string)[]string{
+	ls:=opt.Get(ids...).Sort()
+	res :=make([]string,0)
+	for _,v :=range ls{
+		res=append(res,v.ID)
+	}
+	return res
+}
 
 func New()*Optmizer{
-	return &Optmizer{counter.NewCounter(4000),defaultGetSource}
+	return &Optmizer{counter.NewCounter(4000),defaultGetScore}
 }
